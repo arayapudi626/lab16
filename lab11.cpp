9af442efd937c0aa0fa7c06a0d21a7fbe6007634
arayapudi626 <rayapudianisha@gmail.com>
2025-09-17 07:50:52 -0700

    update

--- lab11.cpp
+++ lab11.cpp
@@ -19,14 +19,24 @@ struct Country {
     };
 
 void getCountry(Country* cptr, int num);
-void displayCountry(Country* cptr, int num);
+void displayCountry(Country* cptr);
 
 int main (){
+
+    Country * list = new Country[COUNTRIES];
+    for (int i = 0; i < COUNTRIES; i++){
+        getCountry(&list[i], i+1);
+    }
+     cout <<'\n'<< "------Country summary-----" << '\n';
+    for (int i = 0; i < COUNTRIES; i++) {
+    displayCountry(&list[i]);
+    }
+    return 0;
 
 }
 
 void getCountry(Country* cptr, int num){
-    cout << "Country #" << num << "\n";
+    cout << "Country #" << num << '\n';
     cout << "Name: ";
     getline(cin, cptr->name);
     cout << "Flight price from US: ";
@@ -35,10 +45,30 @@ void getCountry(Country* cptr, int num){
     cin >> cptr->duration;
     cout <<"How many favorite sights? ";
     cin >> cptr->numSights;
+    cin.ignore();
 
     cptr -> favSights = new string[cptr->numSights];
     for (int i = 0; i < cptr->numSights; i++){
         cout <<"Favorite sight #"<<(i+1)<<": ";
         getline(cin, *(cptr->favSights+i));
     }
+    cin.ignore();
     }
+
+void displayCountry(Country* cptr){
+    cout << "Country: " << cptr -> name << '\n';
+    cout << "Flight price: $" << cptr->flightPrice << '\n';
+    cout << "Days stayed: " << cptr->duration << '\n';
+    cout << "Favorite sights (" << cptr->numSights << "): ";
+    if (cptr->numSights == 0){
+        cout << "no favorite sights :( ";
+    }
+    else {
+        for (int i = 0; i < cptr->numSights; i++)
+        {
+          cout << *(cptr->favSights + i) << " ";
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
