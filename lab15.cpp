0b37bce2b324fe08bc294022f638ed8c5b702671
arayapudi626 <rayapudianisha@gmail.com>
2025-09-25 15:02:49 -0700

    update

--- lab15.cpp
+++ lab15.cpp
@@ -30,5 +30,32 @@ const int SIZE = 4;
  };
 
  int main(){
+    array<Movie, SIZE> movie;
+    ifstream fin ("input.txt");
+    string w;
+    int y;
+    string t;
+    int i = 0;
     
+    if (fin.good()) {
+        while (getline(fin,t)){
+            fin >> y;
+            fin.ignore();
+            getline(fin,w);
+            Movie tmp;
+            tmp.setTitle(t);
+            tmp.setYear(y);
+            tmp.setWriter(w);
+            movie[i] = tmp;
+            i++;
+        }
+        fin.close();
+    }
+    else{
+        cout << "File not found" << endl;
+    }
+    for (auto val : movie){
+        val.print();
+    }
+    return 0;
  }
